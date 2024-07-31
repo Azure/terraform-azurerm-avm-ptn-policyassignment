24ebc0095aa23cc3faae24c0be66b5cc9de03983
u162904 <stephanie.lanius@microsoft.com>
2024-07-31 16:53:28 +0200

    fix: change telemetry similar to alz-ptn

--- main.telemetry.tf
+++ main.telemetry.tf
@@ -1,77 +1,24 @@
-resource "random_id" "telem" {
+data "azurerm_client_config" "telemetry" {
   count = var.enable_telemetry ? 1 : 0
-
-  byte_length = 4
 }
 
-# # This is the module telemetry deployment that is only created if telemetry is enabled.
-# this works for subscription but now for resource group
-resource "azapi_resource" "telemetry" {
-  count = var.enable_telemetry ? 1 : 0
-
-  type = "Microsoft.Resources/deployments@2024-03-01"
-  body = jsonencode({
-    properties = {
-      mode       = "Incremental"
-      parameters = {}
-      template   = jsondecode(local.telem_arm_template_content)
-    }
-  })
-  # location  = var.location # not allowed if scope is resource group
-  location  = var.location
-  name      = local.telem_arm_deployment_name
-  parent_id = var.scope
-  tags      = null
+data "modtm_module_source" "telemetry" {
+  count       = var.enable_telemetry ? 1 : 0
+  module_path = path.module
 }
 
-# resource "azurerm_management_group_template_deployment" "telemetry_mg" {
-#   for_each = tomap({
-#     for vi, v in var.management_group_ids :
-#     vi => v if var.enable_telemetry
-#   })
+resource "random_uuid" "telemetry" {
+  count = var.enable_telemetry ? 1 : 0
+}
 
-#   location            = var.location
-#   management_group_id = each.value
-#   name                = local.telem_arm_deployment_name
-#   tags                = null
-#   template_content    = local.telem_arm_template_content
-# }
+resource "modtm_telemetry" "telemetry" {
+  count = var.enable_telemetry ? 1 : 0
 
-
-# resource "azurerm_subscription_template_deployment" "telemetry_sub" {
-#   for_each = tomap({
-#     for vi, v in var.subscription_ids :
-#     vi => v if var.enable_telemetry
-#   })
-
-#   location         = var.location
-#   name             = local.telem_arm_deployment_name
-#   tags             = null
-#   template_content = local.telem_arm_template_content
-# }
-
-# resource "azurerm_resource_group_template_deployment" "telemetry_rg" {
-#   for_each = tomap({
-#     for vi, v in var.resource_group_ids :
-#     vi => v if var.enable_telemetry
-#   })
-
-#   deployment_mode     = "Incremental"
-#   name                = local.telem_arm_deployment_name
-#   resource_group_name = split("/", each.value)[4]
-#   tags                = null
-#   template_content    = local.telem_arm_template_content
-# }
-
-# resource "azurerm_resource_group_template_deployment" "telemetry_resource" {
-#   for_each = tomap({
-#     for vi, v in var.resource_ids :
-#     vi => v if var.enable_telemetry
-#   })
-
-#   deployment_mode     = "Incremental"
-#   name                = local.telem_arm_deployment_name
-#   resource_group_name = split("/", each.value)[4]
-#   tags                = null
-#   template_content    = local.telem_arm_template_content
-# }
+  tags = {
+    subscription_id = one(data.azurerm_client_config.telemetry).subscription_id
+    tenant_id       = one(data.azurerm_client_config.telemetry).tenant_id
+    module_source   = one(data.modtm_module_source.telemetry).module_source
+    module_version  = one(data.modtm_module_source.telemetry).module_version
+    random_id       = one(random_uuid.telemetry).result
+  }
+}
