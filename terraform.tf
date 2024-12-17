d79fdb141feaf9e9557fdb2239cc30fc72166a5a
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2024-12-17 13:04:19 +0100

    version constraint
    Added a minimum and maximum version of azapi

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = "< 3.0.0"
+      version = ">= 1.14.0, < 3.0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
